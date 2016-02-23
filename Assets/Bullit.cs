607c3e98258344c1e4d64670ba4bb60ca5cab43f
James Gormley <james.gormley@mydit.ie>
2016-02-23 19:50:30 +0000

    Basic Turret with Bullit

---
+++ Assets/Bullit.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullit : MonoBehaviour {
+    //Speed and Range variables. Available in Unity Editor
+    public float bullitSpeed;
+    public float bullitRange;
+    //Distance projectile destroys after, dictated by range variable
+    private float maxDistance;
+
+
+    // Update is called once per frame
+    void Update () {
+        transform.Translate(Vector3.forward * Time.deltaTime * bullitSpeed);
+        maxDistance += Time.deltaTime * bullitSpeed;
+        //Check if bullit is gone past maximum range. Destroy if so
+        if(maxDistance>=bullitRange)
+        {
+            Destroy(gameObject);
+        }
+	
+	}
+}
