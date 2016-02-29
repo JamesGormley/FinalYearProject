88c6a6e472468b1d09e5e35a6c839ae08b2f2c11
James Gormley <james.gormley@mydit.ie>
2016-02-29 19:04:31 +0000

    Menu Page Designed

---
+++ Assets/Scripts/MenuHover.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().material.color = Color.cyan;
+
+    }
+
+    void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = Color.cyan;
+    }
+}
