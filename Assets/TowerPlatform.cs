1f94ce456b65b3ef7f541d09366f555ae67069eb
James Gormley <james.gormley@mydit.ie>
2016-03-01 15:41:24 +0000

    Level 1 and Tower Platforms Created

---
+++ Assets/TowerPlatform.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerPlatform : MonoBehaviour {
+
+
+    public GameObject turret;
+    private Color startcolor;
+
+    //Change colour of platform when mouse hovers over it
+    void OnMouseEnter()
+    {
+        startcolor = GetComponent<Renderer>().material.color;
+        GetComponent<Renderer>().material.color = Color.cyan;
+    }
+
+    //Revert to original colour when mouse leaves object
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = startcolor;
+    }
+
+    //Instantiate tower when platform clicked. 
+    void OnMouseUpAsButton()
+    {
+        // Instantiate tower on clicked platform + 0.2 in y direction for correct height
+        GameObject g = (GameObject)Instantiate(turret);
+        g.transform.position = transform.position + new Vector3(0, 0.2f, 0);
+    }
+
+
+}
