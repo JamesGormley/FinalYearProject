e8c49bfd10ddf581bf3eb468527d88f4aa7d6b3a
James Gormley <james.gormley@mydit.ie>
2016-03-23 19:28:15 +0000

    Upload Scene built but not functional

---
+++ Assets/UploadScoreScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+//This script will deal with taking the players name from an input field and their score from the game and uploading them both to the db
+public class UploadScoreScript : MonoBehaviour {
+    
+    //Maximum allowed length of name. 16 is the value of the varchar in the db
+    private int maxNameLength = 16;
+
+    //Variable to hold username which will be uploaded with score. Begins empty
+    private string userName = string.Empty;
+
+    private int userScore;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
